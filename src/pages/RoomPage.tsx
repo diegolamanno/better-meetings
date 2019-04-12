391c6cbe8f33705e906733c2258f8113638c273c
David D'Amico <david.damico@ecobee.com>
2019-04-12 10:08:20 -0400

    :construction: added basic handler for state change request

--- src/pages/RoomPage.tsx
+++ src/pages/RoomPage.tsx
@@ -17,6 +17,13 @@ const RoomPage: FC<RouteComponentProps> = () => {
 		.pop()!
 		.split('.')
 		.pop() as State
+	const handleStateRepresenationTapped = (
+		e: SyntheticEvent<Element, Event>,
+	) => {
+		e.preventDefault()
+		// TODO handle user's state change
+		// console.log('received the request for state change')
+	}
 	return (
 		<Fragment>
 			<Global
@@ -33,7 +40,11 @@ const RoomPage: FC<RouteComponentProps> = () => {
 				`}
 			>
 				<p>welcome to the room: {roomId}!</p>
-				<div className="state-container">
+				<div
+					className="state-container"
+					onClick={handleStateRepresenationTapped}
+					onTouchEnd={handleStateRepresenationTapped}
+				>
 					<UserRoomState
 						state={
 							roomMachine.state.matches('subscribed') &&
