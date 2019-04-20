bc2744bc483bd633034c52dd8fb8b4cdab9479f8
Diego La Manno <diegom@ecobee.com>
2019-04-20 15:13:33 -0400

    🔨 Redirected all routes to failure if they don't exist

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { hot } from 'react-hot-loader'
 import { Router } from '@reach/router'
+import { RouteComponentProps } from '@reach/router'
 import App from './App'
 import Callback from './Callback'
 import Login from './Login'
-import HomePage from '../pages/HomePage'
 import JoinRoomPage from '../pages/JoinRoomPage'
 import RoomPage from '../pages/RoomPage'
+
+const NotFound: FC<RouteComponentProps> = () => <div>Sorry, nothing here.</div>
 
 const Routes = () => (
 	<Router>
 		<Callback path="/callback" />
 		<Login path="/" />
 		<App path="home">
-			<HomePage path="home-legacy" />
 			<JoinRoomPage path="room/join" />
 			<RoomPage path="room/:roomId" />
 		</App>
+		<NotFound default />
 	</Router>
 )
 
